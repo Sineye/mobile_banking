4d0d0eabed936b4dc4cd747877313130023ef9e9
SpontanCombust <cedro_p@o2.pl>
2021-12-11 14:01:00 +0100

    drobny refactor

--- lib/fingerprint_login_screen.dart
+++ lib/fingerprint_login_screen.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
-class FingerprintLoginScreen extends StatelessWidget {
+class FingerprintLoginScreen extends StatefulWidget {
   const FingerprintLoginScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Text('Przyłóż palec do czytnika odcisków.')
-    );
+  State<FingerprintLoginScreen> createState() => FingerprintLoginScreenState();
+}
+
+class FingerprintLoginScreenState extends State<FingerprintLoginScreen> {
+
+  final LocalAuthentication localAuthentication = LocalAuthentication();
+
+  Future<bool> checkingForBioMetrics() async {
+    bool canCheckBiometrics = await localAuthentication.canCheckBiometrics;
+    return canCheckBiometrics;
   }
 
+  Future<void> _authenticateMe() async {
+    bool authenticated = false;
+    try {
+      authenticated = await localAuthentication.authenticate(
+        biometricOnly: true,
+        localizedReason: "Authenticate", // message for dialog
+        useErrorDialogs: true, // show error in dialog
+        stickyAuth: true, // native process
+      );
+      if (authenticated)
+      {
+        Navigator.pushReplacementNamed(context, '/account_state');
+      }
+    } catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+  }
+
+void func() async {
+  _authenticateMe();
+}
+
+  @override
+  Widget build(BuildContext context) {
+    func();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+          children: [Center(child: Padding(
+            padding: const EdgeInsets.all(64.0),
+            child: Text('Use your fingerprint scanner to login.'),
+          ))])
+    );
+  }
 }
--- lib/password_login_screen.dart
+++ lib/password_login_screen.dart
@@ -1,14 +1,85 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class PasswordLoginScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PasswordLoginScreen extends StatefulWidget {
   const PasswordLoginScreen({Key? key}) : super(key: key);
 
   @override
+  State<PasswordLoginScreen> createState() => PasswordLoginScreenState();
+}
+
+class PasswordLoginScreenState extends State<PasswordLoginScreen> {
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController pinController = TextEditingController();
+  bool failed = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Text('Podaj hasło.')
-    );
+    if (failed)
+      {
+        return Scaffold(
+            appBar: AppBar(),
+            body: Padding(
+              padding: const EdgeInsets.all(64.0),
+              child: Column(
+                  children: [Center(child: Text('Enter your login and PIN.')),
+                    TextField(obscureText: false,controller: usernameController,),
+                    TextField(obscureText: true, keyboardType: TextInputType.number, controller: pinController,),
+                    Padding(
+                      padding: const EdgeInsets.all(64.0),
+                      child: OutlinedButton(child: Text('Accept',textAlign: TextAlign.center), onPressed: checkCredentials,),
+                    ), Text('Invalid credentials.')]),
+            )
+        );
+      }
+
+    else{
+      return Scaffold(
+          appBar: AppBar(),
+          body: Padding(
+            padding: const EdgeInsets.all(64.0),
+            child: Column(
+                children: [Center(child: Text('Enter your login and PIN.')),
+                  TextField(obscureText: false,controller: usernameController,),
+                  TextField(obscureText: true, keyboardType: TextInputType.number, controller: pinController,),
+                  Padding(
+                    padding: const EdgeInsets.all(64.0),
+                    child: OutlinedButton(child: Text('Accept',textAlign: TextAlign.center), onPressed: checkCredentials,),
+                  )]),
+          )
+      );
+    }
+
   }
 
+  @override
+  void dispose() {
+    usernameController.dispose();
+    pinController.dispose();
+    super.dispose();
+  }
+
+  void checkCredentials() async {
+    final response = await http.post(Uri.parse('http://10.0.2.2:9001/users/0'), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+         body: jsonEncode({
+      'login': usernameController.text,
+      'pin': pinController.text
+    }));
+
+    if (response.statusCode == 200) {
+      Navigator.pushReplacementNamed(context, '/account_state');
+    }
+
+    else
+    {
+      setState(() {
+        failed = true;
+      });
+    }
+  }
 }
